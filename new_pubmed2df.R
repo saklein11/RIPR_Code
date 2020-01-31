cdd09bc6fdaa0c1d121796d4b4426f7e9fc24961
Allison Kolbe <31106309+arkolbe@users.noreply.github.com>
2020-01-31 09:28:21 -0500

    initial commit
    altered pubmed2df function

---
+++ new_pubmed2df.R
@@ -0,0 +1,72 @@
+
+pubmed2df<-function(D){
+  
+  records=D
+  rm(D)
+  ## Author 
+  AU=unlist(lapply(Author(records),function(a){
+    paste(paste(a$LastName,a$Initials,sep=" "),collapse =";")}))
+  
+  ## Total citations
+  #cat("\nDownloading updated citations from PubMed/MedLine...\n\n")
+  TC=NA
+  
+  
+  ## Country
+  AU_CO=Country(records)
+  
+  ## DOI
+  DI=ELocationID(records)
+  
+  ## Source ISO
+  JI=ISOAbbreviation(records)
+  
+  ## ISSN
+  ISSN=ISSN(records)
+  
+  ## Volume
+  VOL=Volume(records)
+  
+  ## Issue
+  ISSUE=Issue(records)
+  
+  ## Language
+  LT=Language(records)
+  
+  ## Affiliation
+  AFF=unlist(lapply(Affiliation(records),function(a){
+    paste(a,collapse =";")}))
+  
+  ## Title
+  TI=ArticleTitle(records)
+  
+  ## Abstract
+  AB=AbstractText(records)
+  
+  ## Pub year
+  PY=YearPubmed(records)
+  
+  ## Pub type
+  DT=unlist(lapply(PublicationType(records),function(a){
+    paste(a,collapse =";")}))
+  
+  ## Article ID
+  UT=ArticleId(records)
+  
+  ## Mesh
+  MESH=unlist(lapply(Mesh(records),function(a){
+    if (is.data.frame(a)){
+      a=paste(a$Heading,collapse =";")}else{a='NA'}
+  }))
+  
+  
+  DATA <- data.frame('AU'=AU, 'TI'=TI,'AB'=AB,'PY'=PY, 'DT'=DT, 
+                     'MESH'=MESH, 'TC'=TC, 'SO'=JI, 'J9'=JI, 'JI'=JI, 'DI'=DI,'ISSN'=ISSN, 
+                     'VOL'=VOL, 'ISSUE'=ISSUE, 'LT'=LT, 'C1'=AFF, 'RP'=AFF, 'ID'=MESH,'DE'=MESH,
+                     'UT'=UT, 'AU_CO'=AU_CO, stringsAsFactors = FALSE)
+  DATA <- data.frame(lapply(DATA,toupper),stringsAsFactors = FALSE)
+  DATA$DB = "PUBMED"
+  
+  
+  return(DATA)
+}
